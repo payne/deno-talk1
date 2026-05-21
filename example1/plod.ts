7a4dba525752e5b5310d627dc14f5c317f6f36c0
Matt Payne <Payne@MattPayne.org>
2026-05-21 06:03:41 -0500

    deno fmt plod.ts

--- example1/plod.ts
+++ example1/plod.ts
@@ -2,24 +2,26 @@ import { format } from "jsr:@std/internal@^1.0.5/format";
 
 const currentNow = now();
 const entry = from(Deno.args) ?? from(currentNow);
-await Deno.writeTextFile("log.txt", `${currentNow} --\n${entry}\n`, { append: true });
+await Deno.writeTextFile("log.txt", `${currentNow} --\n${entry}\n`, {
+  append: true,
+});
 
 function from(param: string | string[]) {
-if (Array.isArray(param)) {
-  return param.length > 0 ? param.join(' ') : undefined
-}
+  if (Array.isArray(param)) {
+    return param.length > 0 ? param.join(" ") : undefined;
+  }
   const currentNow = param;
   console.log(`${currentNow} --`);
   return prompt(``);
 }
 
-function now(): string { 
-return new Date().toLocaleString("en-US", {
-  month: "2-digit",
-  day: "2-digit",
-  year: "numeric",
-  hour: "2-digit",
-  minute: "2-digit",
-  hourCycle: "h23",
-});
+function now(): string {
+  return new Date().toLocaleString("en-US", {
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hourCycle: "h23",
+  });
 }
