d07cb7aa1375d4fdad201c114e462a403318afc9
Matt Payne <Payne@MattPayne.org>
2026-05-21 06:03:10 -0500

    fixed logic error where I was using the variable now too much and was getting the function a bunch

--- example1/plod.ts
+++ example1/plod.ts
@@ -1,15 +1,20 @@
 import { format } from "jsr:@std/internal@^1.0.5/format";
 
+const currentNow = now();
+const entry = from(Deno.args) ?? from(currentNow);
+await Deno.writeTextFile("log.txt", `${currentNow} --\n${entry}\n`, { append: true });
+
 function from(param: string | string[]) {
 if (Array.isArray(param)) {
-  return param.length > 1 ? param.join(' ') : undefined
+  return param.length > 0 ? param.join(' ') : undefined
 }
-  const now = param;
-  console.log(`${now} --`);
+  const currentNow = param;
+  console.log(`${currentNow} --`);
   return prompt(``);
 }
 
-const now = new Date().toLocaleString("en-US", {
+function now(): string { 
+return new Date().toLocaleString("en-US", {
   month: "2-digit",
   day: "2-digit",
   year: "numeric",
@@ -17,5 +22,4 @@ const now = new Date().toLocaleString("en-US", {
   minute: "2-digit",
   hourCycle: "h23",
 });
-const entry = from(Deno.args) ?? from(now);
-await Deno.writeTextFile("log.txt", `${now} --\n${entry}\n`, { append: true });
+}
